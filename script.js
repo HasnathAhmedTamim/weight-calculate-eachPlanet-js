5c1eb930cbce5a3c86f568d37fe4d2a4f708f55c
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2024-01-06 22:32:34 +0600

    add button grid flex hover

---
+++ script.js
@@ -0,0 +1,14 @@
+// Gravitational acc on each Planet
+
+const gravitationalAcc = {
+  mercury: 3.7,
+  venus: 8.87,
+  earth: 9.81,
+  mars: 3.71,
+  jupiter: 24.79,
+  saturn: 10.44,
+  uranus: 8.69,
+  neptune: 11.15,
+};
+
+// cal funct for each planet
