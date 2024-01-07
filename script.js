9247fcca626e6b5af6617749b6ead030f301f5c5
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2024-01-07 19:31:56 +0600

    done

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Gravitational acc on each Planet
-
 const gravitationalAcc = {
   mercury: 3.7,
   venus: 8.87,
@@ -11,20 +10,69 @@ const gravitationalAcc = {
   neptune: 11.15,
 };
 
+// Image URLs for each planet
+const planetImages = {
+  mercury: "/assets/mercury.jpg",
+  venus: "/assets/venus.jpg",
+  earth: "/assets/earth.jpg",
+  mars: "/assets/mars.jpg",
+  jupiter: "/assets/jupiter.jpg",
+  saturn: "/assets/saturn.jpg",
+  uranus: "/assets/uranus.jpg",
+  neptune: "/assets/neptune.jpg",
+};
 // cal function for each planet
 
+// function calWeightOnPlanets(weightOnEarth) {
+//   const weightOnPlanets = {};
+//   for (const planet in gravitationalAcc) {
+//     const gravityOnPlanet = gravitationalAcc[planet];
+//     const weightOnPlanet =
+//       (weightOnEarth * gravityOnPlanet) / gravitationalAcc.earth;
+//     weightOnPlanets[planet] = weightOnPlanet;
+//   }
+//   return weightOnPlanets;
+// }
+// cal function for each planet
 function calWeightOnPlanets(weightOnEarth) {
   const weightOnPlanets = {};
   for (const planet in gravitationalAcc) {
     const gravityOnPlanet = gravitationalAcc[planet];
     const weightOnPlanet =
       (weightOnEarth * gravityOnPlanet) / gravitationalAcc.earth;
-    weightOnPlanets[planet] = weightOnPlanet;
+    weightOnPlanets[planet] = {
+      weight: weightOnPlanet,
+      image: planetImages[planet],
+    };
   }
   return weightOnPlanets;
 }
 
 // function for handle btn , display res
+// function calculateWeight() {
+//   const earthWeightInput = document.getElementById("earthWeight");
+//   const resultDiv = document.getElementById("result");
+
+//   const weightOnEarth = parseFloat(earthWeightInput.value);
+
+//   if (!isNaN(weightOnEarth)) {
+//     const weightOnPlanets = calWeightOnPlanets(weightOnEarth);
+
+//     let resultText = ``;
+//     for (const planet in weightOnPlanets) {
+//       resultText += `<div class="planet-box">
+
+//         <p>${
+//           planet.charAt(0).toUpperCase() + planet.slice(1)
+//         }<br>${weightOnPlanets[planet].toFixed(2)}kg</p>
+//       </div>`;
+//     }
+//     resultDiv.innerHTML = resultText;
+//   } else {
+//     resultDiv.innerHTML = "<p> Enter Your Valid Weight.</p>";
+//   }
+// }
+// function for handle btn, display res
 function calculateWeight() {
   const earthWeightInput = document.getElementById("earthWeight");
   const resultDiv = document.getElementById("result");
@@ -37,15 +85,15 @@ function calculateWeight() {
     let resultText = ``;
     for (const planet in weightOnPlanets) {
       resultText += `<div class="planet-box">
-        <img src=${planet}.png />
+        <img src="${weightOnPlanets[planet].image}" alt="${planet}" />
         <p>${
           planet.charAt(0).toUpperCase() + planet.slice(1)
-        }<br>${weightOnPlanets[planet].toFixed(2)}kg</p>
+        }<br>${weightOnPlanets[planet].weight.toFixed(2)}kg</p>
       </div>`;
     }
     resultDiv.innerHTML = resultText;
   } else {
-    resultDiv.innerHTML = "<p> Enter Your Valid Weight.</p>";
+    resultDiv.innerHTML = "<p>Enter Your Valid Weight.</p>";
   }
 }
 
